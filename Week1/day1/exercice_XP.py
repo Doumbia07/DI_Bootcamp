8992ca3d653f790937131620f128a165716aee6d
Doumbia Abou-Bakar Sidik <doumbiaaboubakarsidik0759@gmail.com>
2026-05-20 00:27:56 +0000

    second commit

--- Week1/day1/exercice_XP.py
+++ Week1/day1/exercice_XP.py
@@ -1,50 +1,50 @@
-# Exercice 1 : Hello World
-print("Hello world\n" * 4)
-# Exercice 2 : Some Math
+# EXERCICE 1
+print("Hello world\nHello world\nHello world\nHello world")
+
+# EXERCICE 2
 result = (99 ** 3) * 8
 print(result)
 
-# Exercice 3 : What is the output ?
+# EXERCICE 3
+print(5 < 3)          # False
+print(3 == 3)         # True
+print(3 == "3")       # False
+print("3" > 3)        # TypeError
+print("Hello" == "hello")  # False
 
-print(5 < 3) #False
-print(3 == 3) #True
-print(3 == "3") #False
-print("3" > 3)   #TypeError
-print("Hello" == "hello") #False
-
-# Exercice 4 : Your computer brand
-computer_brand = "Toshiba"   
+# EXERCICE 4
+computer_brand = "Toshiba"
 print("I have a " + computer_brand + " computer.")
 
-# Exercice 5 : Your information
+# EXERCICE 5
 name = "Doumbia"
 age = 26
 shoe_size = 44
 info = f"Je m'appelle {name}, j'ai {age} ans et je chausse du {shoe_size}. J'adore programmer !"
 print(info)
 
-# Exercice 6 : A & B
+# EXERCICE 6
 a = 12
 b = 8
 if a > b:
     print("Hello World")
 
-# Exercice 7 : Odd or Even
+# EXERCICE 7
 nombre = int(input("Donne un nombre entier : "))
 if nombre % 2 == 0:
     print("Pair")
 else:
     print("Impair")
 
-# Exercice 8 : What’s your name ?
+# EXERCICE 8
 mon_nom = "Doumbia"
 nom_utilisateur = input("Quel est ton nom ? ")
 if mon_nom.lower() == nom_utilisateur.lower():
-    print("On a le même nom")
+    print("On a le même nom !")
 else:
     print("Salut " + nom_utilisateur + ", moi c'est " + mon_nom + ".")
 
-# Exercice 9 : Tall enough to ride a roller coaster
+# EXERCICE 9
 taille_cm = int(input("Taille en centimètres : "))
 if taille_cm > 145:
     print("Tu es assez grand pour le manège.")
